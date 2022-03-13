6c84db7fa93428bb3ac63f90e48846a30a8524c5
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2022-03-13 16:06:43 +0200

    Deleted the admin middleware

--- routes/genres.js
+++ routes/genres.js
@@ -1,51 +1,52 @@
-
-const auth = require('../middleware/auth');
-const admin = require('../middleware/admin');
-const express = require('express');
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+const express = require("express");
 const router = express.Router();
-const { Genre } = require('../models/genre');
+const { Genre } = require("../models/genre");
 
-router.get('/', async (req, res, next) => {
-    const genres = await Genre.find().sort('name');
-    res.send(genres);
+router.get("/", async (req, res, next) => {
+  const genres = await Genre.find().sort("name");
+  res.send(genres);
 });
 
-router.get('/:id', async (req, res, next) => {
-    const genre = await Genre.findById(req.params.id);
+router.get("/:id", async (req, res, next) => {
+  const genre = await Genre.findById(req.params.id);
+  res.send(genre);
+});
+
+router.post("/", auth, async (req, res) => {
+  try {
+    if (!req.body.name)
+      return res.status(400).send("Bad request. Genre needs a name");
+    let genre = new Genre({ name: req.body.name });
+    genre = await genre.save();
     res.send(genre);
+  } catch (ex) {
+    for (let field in ex.errors) res.status(400).send(ex.errors[field].message);
+  }
 });
 
-router.post('/', auth, async (req, res)=> {
-    try {
-        if (!req.body.name) return res.status(400).send('Bad request. Genre needs a name');
-        let genre = new Genre({name: req.body.name});
-        genre = await genre.save();
-        res.send(genre);
-    } catch(ex) {
-        for (let field in ex.errors) res.status(400).send(ex.errors[field].message);
-    }
+router.put("/:id", auth, async (req, res) => {
+  try {
+    if (!req.body.name)
+      return res.status(400).send("Bad request. Genre needs a name");
+    let genre = await Genre.findById(req.params.id);
+    genre.set({ name: req.body.name });
+    await genre.save();
+    res.send(genre);
+  } catch (ex) {
+    if (ex.kind) return res.status(400).send("No such Genre");
+    for (let field in ex.errors) res.status(400).send(ex.errors[field].message);
+  }
 });
 
-router.put('/:id', auth, async (req, res) => {
-    try {
-        if (!req.body.name) return res.status(400).send('Bad request. Genre needs a name');
-        let genre = await Genre.findById(req.params.id);
-        genre.set({ name: req.body.name });
-        await genre.save();
-        res.send(genre);
-    } catch(ex) {
-        if (ex.kind) return res.status(400).send('No such Genre');
-        for (let field in ex.errors) res.status(400).send(ex.errors[field].message);
-    }
-});
-
-router.delete('/:id', [auth, admin], async (req, res) => {
-    try {
+router.delete("/:id", auth, async (req, res) => {
+  try {
     const genre = await Genre.findByIdAndRemove(req.params.id);
     res.send(genre);
-    } catch(ex) {
-        if (ex.kind) return res.status(400).send('No such Genre!!!');
-    }
+  } catch (ex) {
+    if (ex.kind) return res.status(400).send("No such Genre!!!");
+  }
 });
 
 module.exports = router;
--- routes/movies.js
+++ routes/movies.js
@@ -1,79 +1,81 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const auth = require('../middleware/auth');
-const admin = require('../middleware/admin');
-const { Genre } = require('../models/genre');
-const { Movie } = require('../models/movie');
+const mongoose = require("mongoose");
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+const { Genre } = require("../models/genre");
+const { Movie } = require("../models/movie");
 
-router.get('/', async (req, res) => {
-    const movies = await Movie.find().sort('name');
-    res.send(movies);
+router.get("/", async (req, res) => {
+  const movies = await Movie.find().sort("name");
+  res.send(movies);
 });
 
-router.get('/:id', async (req, res) => {
-    try {
-      const movie = await Movie.findById(req.params.id);
-      res.send(movie);
-    } catch (ex) {
-      if (ex) res.status(404).send('No such Movie');
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    res.send(movie);
+  } catch (ex) {
+    if (ex) res.status(404).send("No such Movie");
+  }
+});
 
-})
+router.post("/", [auth], async (req, res) => {
+  if (!req.body.title) res.status(400).send("Bad request. Movie needs a title");
 
-router.post('/', [auth], async (req, res) => {
-  if (!req.body.title) res.status(400).send('Bad request. Movie needs a title');
-  
   const genre = await Genre.findById(req.body.genre);
-  if (!genre) return res.status(400).send('Invalid genre');
-  
+  if (!genre) return res.status(400).send("Invalid genre");
+
   let movie = await new Movie({
     title: req.body.title,
     genre: {
       _id: genre._id,
-      name: genre.name
+      name: genre.name,
     },
     numberInStock: req.body.numberInStock,
     dailyRentalRate: req.body.dailyRentalRate,
-    liked: req.body.liked
+    liked: req.body.liked,
   });
-  
+
   movie = await movie.save();
-  
+
   res.send(movie);
 });
 
-router.put('/:id', [auth, admin], async (req, res) => {
-  if (!req.body.title) res.status(400).send('Bad request. Movie needs a title');
-  
+router.put("/:id", auth, async (req, res) => {
+  if (!req.body.title) res.status(400).send("Bad request. Movie needs a title");
+
   const genre = await Genre.findById(req.body.genre);
-      if (!genre) return res.status(400).send('Invalid genre');
+  if (!genre) return res.status(400).send("Invalid genre");
 
-      const movie = await Movie.findByIdAndUpdate(req.params.id, { 
-        title: req.body.title,
-        genre: {
-          _id: genre._id,
-          name: genre.name
-        },
-        numberInStock: req.body.numberInStock,
-        dailyRentalRate: req.body.dailyRentalRate,
-        liked: req.body.liked
-      }, { new: true });
-      
-    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
-    
-    res.send(movie);
+  const movie = await Movie.findByIdAndUpdate(
+    req.params.id,
+    {
+      title: req.body.title,
+      genre: {
+        _id: genre._id,
+        name: genre.name,
+      },
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate,
+      liked: req.body.liked,
+    },
+    { new: true }
+  );
 
+  if (!movie)
+    return res.status(404).send("The movie with the given ID was not found.");
 
-
+  res.send(movie);
 });
 
-router.delete('/:id', [auth, admin], async (req, res) => {
-    const movie = await Movie.findByIdAndRemove(req.params.id);
-  
-    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
-  
-    res.send(movie);
-  });
+router.delete("/:id", auth, async (req, res) => {
+  const movie = await Movie.findByIdAndRemove(req.params.id);
 
-module.exports = router; 
+  if (!movie)
+    return res.status(404).send("The movie with the given ID was not found.");
+
+  res.send(movie);
+});
+
+module.exports = router;
